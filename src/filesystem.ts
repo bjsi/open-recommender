f44a480b0a63d60af2271ada394d8d871ec0b115
Jamesb <experimentallearning0@gmail.com>
2023-12-02 18:12:42 +0000

    add dbPath

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -2,3 +2,4 @@ import path from "path";
 
 export const dataFolder = path.join(__dirname, "..", "data");
 export const testDataFolder = path.join(__dirname, "recommender/tests/data");
+export const dbPath = path.join(dataFolder, "db.sqlite");
